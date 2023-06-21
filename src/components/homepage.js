e705e746c98f88dd9e7e0d34b2e6c064935f4b03
Han <hantigre@hotmail.com>
2023-06-20 22:03:01 -0400

    changed some words

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -16,14 +16,14 @@ export default function Homepage() {
                 <div id="text">
                 👋 Hi, I'm Han, a final-year Computer Science student @ <a className="links" href="https://www.umontreal.ca" target="_blank" rel="noopener noreferrer">Université de Montréal.</a>
                 <br></br>
-                I'm currently working as a Technology Analyst at Morgan Stanley in the App Services Infra team.
+                I'm currently working as a Technology Analyst @ <a className="links" href="https://www.morganstanley.com/" target="_blank" rel="noopener noreferrer">Morgan Stanley</a> in the App Services Infra team.
                 <br></br>
                 I was previously a DevOps Intern @ <a className="links" href="https://www.nuance.com/index.html" target="_blank" rel="noopener noreferrer">Nuance Communications</a> working in the CoreTech Infra team
-                and a Software Developer Intern @ <a className="links" href="https://www.bce.ca/about-bce/bce-overview" target="_blank" rel="noopener noreferrer">Bell Canada</a> working on hybrid cloud infrastructure.
+                and a Software Developer Intern @ <a className="links" href="https://www.bce.ca/about-bce/bce-overview" target="_blank" rel="noopener noreferrer">Bell Canada</a> working in the Hybrid Cloud team.
                 <br></br>
                 I'm a fast learner who likes solving interesting problems with code.
                 <br></br>
-                Outside of tech, I also like fashion and taking <a className="links" href="https://www.instagram.com/hanzhg" target="_blank" rel="noopener noreferrer">photos</a>.
+                Outside of tech, I'm also interested in fashion and taking <a className="links" href="https://www.instagram.com/hanzhg" target="_blank" rel="noopener noreferrer">photos</a>.
                 </div>
             </div>
             <Clock />
